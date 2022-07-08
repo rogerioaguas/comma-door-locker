c8c19764044b6c9908709f7b2a05f760361da50d
Bruno Colombini <bruno.colombini@me.com>
2022-07-08 14:24:56 -0300

    remove obvious url

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -30,7 +30,8 @@ export function register(config) {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${process.env.PUBLIC_URL}/comma-door-locker/service-worker.js`;
+            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+            console.log(process.env.PUBLIC_URL)
 
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
